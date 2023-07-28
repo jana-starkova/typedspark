77a588c8783008a946e0de52eac90bccda1d1a93
Jana Starkova <jana@kaiko.ai>
2023-07-28 14:01:42 +0200

    include docstrings in string schema

--- typedspark/_schema/get_schema_definition.py
+++ typedspark/_schema/get_schema_definition.py
@@ -89,3 +89,58 @@ def _add_subschemas(schema: Type[Schema], add_subschemas: bool, include_document
             )
 
     return lines
+
+
+def _replace_literals(
+    typehint: str,
+    replace_literals_in: Type[TypedSparkDataType],
+    replace_literals_by: Type[LiteralType],
+) -> str:
+    """Replace all Literals in a LiteralType, e.g.
+
+    "DayTimeIntervalType[Literal[0], Literal[1]]" ->
+    "DayTimeIntervalType[IntervalType.DAY, IntervalType.HOUR]"
+    """
+    mapping = replace_literals_by.get_inverse_dict()
+    for original, replacement in mapping.items():
+        typehint = _replace_literal(typehint, replace_literals_in, original, replacement)
+
+    return typehint
+
+
+def _replace_literal(
+    typehint: str,
+    replace_literals_in: Type[TypedSparkDataType],
+    original: str,
+    replacement: str,
+) -> str:
+    """Replaces a single Literal in a LiteralType, e.g.
+
+    "DayTimeIntervalType[Literal[0], Literal[1]]" ->
+    "DayTimeIntervalType[IntervalType.DAY, Literal[1]]"
+    """
+    return re.sub(
+        rf"{replace_literals_in.get_name()}\[[^]]*\]",
+        lambda x: x.group(0).replace(original, replacement),
+        typehint,
+    )
+
+
+def _add_subschemas(schema: Type[Schema], add_subschemas: bool, include_documentation: bool) -> str:
+    """Identifies whether any ``Column`` are of the ``StructType`` type and
+    generates their schema recursively."""
+    lines = ""
+    for val in get_type_hints(schema).values():
+        args = get_args(val)
+        if not args:
+            continue
+
+        dtype = args[0]
+        if get_origin(dtype) == StructType:
+            lines += "\n\n"
+            subschema: Type[Schema] = get_args(dtype)[0]
+            lines += _build_schema_definition_string(
+                subschema, include_documentation, add_subschemas, subschema.get_schema_name()
+            )
+
+    return lines
